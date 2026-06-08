26352dd8c62c8f8f6974d53a86b3d19b1947c9eb
orzechsonmichael <herinpl@gmail.com>
2026-06-08 17:49:31 +0200

    Update flight_tracker.py

--- flight_tracker.py
+++ flight_tracker.py
@@ -1,14 +1,17 @@
 import os
 import requests
 
-webhook = os.environ["DISCORD_WEBHOOK_URL"]
+api_key = os.environ["RAPIDAPI_KEY"]
+api_host = os.environ["RAPIDAPI_HOST"]
 
-requests.post(
-    webhook,
-    json={
-        "content": "🇯🇵 Flight Tracker działa! Pierwszy test z GitHub Actions."
-    },
-    timeout=30,
-)
+url = "https://aerodatabox.p.rapidapi.com/airports/IATA/WAW"
 
-print("Wysłano wiadomość na Discord.")
+headers = {
+    "X-RapidAPI-Key": api_key,
+    "X-RapidAPI-Host": api_host
+}
+
+response = requests.get(url, headers=headers, timeout=30)
+
+print("Status:", response.status_code)
+print(response.text[:500])
