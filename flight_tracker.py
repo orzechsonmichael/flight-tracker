2b6b2f98b67b5f63cbae95b36d2e8463f7c6acc1
orzechsonmichael <herinpl@gmail.com>
2026-06-08 17:18:44 +0200

    Update flight_tracker.py

--- flight_tracker.py
+++ flight_tracker.py
@@ -1,1 +1,14 @@
-import test_request
+import os
+import requests
+
+webhook = os.environ["DISCORD_WEBHOOK_URL"]
+
+requests.post(
+    webhook,
+    json={
+        "content": "🇯🇵 Flight Tracker działa! Pierwszy test z GitHub Actions."
+    },
+    timeout=30,
+)
+
+print("Wysłano wiadomość na Discord.")
